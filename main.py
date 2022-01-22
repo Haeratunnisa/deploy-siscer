fbc635350368de384f757bb8c125d631ca77a954
Haeratunnisa <85877386+Haeratunnisa@users.noreply.github.com>
2022-01-22 18:23:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,99 +5,91 @@ import numpy as np
 from sklearn.model_selection import train_test_split 
 
 st.write("""
-    # Program Prediksi Peluang Penerimaan Program S2
+    # Prediksi Stroke
 """)
 
 st.image('./image.jpg')
 
 st.write( """
-    ## Keterangan Data Yang Digunakan
-    1. GRE Score: Merupakan Score Test Untuk Masuk Program S2 (0 - 340) Bersifat Continous
-    2. TOEFL Score: Score Kemampuan TOEFL (0 - 120) Bersifat Continous
-    3. University Rating: Rating Universitas (0 - 5) Bersifat Ordinal
-    4. Kekuatan Surat Rekomendasi (0 - 5) Bersifat Ordinal
-    5. GPA Sewaktu Undergraduate (0 - 10) Bersifat Continous
-    6. Pengalaman Riset (0 : tidak ada, 1 : ada) Bersifat Nominal
-    
-    7. Peluang Diterima (0 - 1) Merupakan Dependent Variable
+    ## Keterangan Data Yang Diinputkan
+    1. Gender: Jenis kelamin
+    2. Age: Umur
+    3. Hypertension:
+       0 = tidak memimiliki hipertensi
+       1 = memiliki hipertensi
+    4. Heart Disease:
+       0 = tidak memiliki hipertensi
+       1 = memiliki hipertensi
+    5. Average Glucose Level: Rata-rata kadar glukosa dalam darah
+    6. BMI: Body mass index
 
 """)
 
-st.write("""
-    ## Overview Data
-""")
+#importing libraries
 
-myData = pd.read_csv('data.csv')
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.tree import DecisionTreeClassifier
 
-st.dataframe(myData)
+df = pd.read_csv('stroke.csv')
 
-st.write("""
-    ## Deskripsi Data
-""")
+# replace data kosong
+df['bmi'].fillna(df['bmi'].mean(), inplace = True)
 
-st.dataframe(myData.describe())
+# hapus kolom id
+df.drop('id', axis = 1,inplace = True)
 
-# Preproccessing Data
+# handling categorical features
+en = LabelEncoder()
 
-st.write("""
-    ## Dilakukan Preprocessing Data dimana Fitur dan Labelnya akan Dipisah
-""")
+cols = ['gender']
+for col in cols:
+  df[col] = en.fit_transform(df[col])
 
-# Memisahkan Label Dan Fitur 
-X = myData.iloc[:, 1:-1].values
-y = myData.iloc[:, -1].values
+# handling imbalanced data
+x = df.drop(['stroke', 'ever_married', 'work_type', 'Residence_type', 'smoking_status'], axis = 'columns')
+y = df['stroke']
 
-
-
-st.write("## Input Data X",X)
-st.write("## Label Data y",y)
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
-
-
+# train test split
+X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.2, random_state=69)
 
 from sklearn.preprocessing import StandardScaler 
 
 ss_train_test = StandardScaler()
 
-
 X_train_ss_scaled = ss_train_test.fit_transform(X_train)
 X_test_ss_scaled = ss_train_test.transform(X_test)
 
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+# decision tree classifier
+m = DecisionTreeClassifier()
+m.fit(X_train_ss_scaled, y_train)
+y_pred_dt = m.predict(X_test)
 
-l_regressor_ss = LinearRegression()
-l_regressor_ss.fit(X_train_ss_scaled, y_train)
-y_pred_l_reg_ss = l_regressor_ss.predict(X_test_ss_scaled)
+st.write("Dengan Menggunakan Deciision Tree Nilai Akurasinya Adalah:")
+st.write(accuracy_score(y_test, y_pred_dt))
 
-st.write("Dengan Menggunakan Multiple Linear Regression Diperoleh Skor Untuk Data Test")
-st.write(r2_score(y_test, y_pred_l_reg_ss))
-
-
-st.write("# Sekarang Silahkan Masukan Skor Test Kamu Untuk Mengetahui Prediksi Peluang Kelulusan S2 Kamu")
+st.write("# Masukkan data")
 
 
 form = st.form(key='my-form')
-inputGRE = form.number_input("Masukan GRE Score: ", 0)
-inputTOEFL = form.number_input("Masukan TOEFL Score: ", 0)
-inputUnivRating = form.number_input("Masukan Rating Univ: ", 0)
-inputSOP = form.number_input("Masukan Kekuatan SOP: ", 0)
-inputLOR = form.number_input("Masukan Kekuatan LOR: ", 0)
-inputCGPA = form.number_input("Masukan CGPA: ", 0)
-inputResearch = form.number_input("Pengalaman Researc, 1 Jika Pernah Riset, 0 Jika Tidak", 0)
+inputGender = form.number_input("Jenis kelamin: ", 0)
+inputAge = form.number_input("Umur: ", 0)
+inputHyper = form.number_input("Apakah mempunyai hipertensi? (1 = ya, 0 = tidak): ", 0)
+inputHD = form.number_input("Apakah mempunyai penyakit jantung (1 = ya, 0 = tidak): ", 0)
+inputGlucose = form.number_input("Rata-rata kadar glukosa: ", 0)
+inputBMI = form.number_input("BMI: ", 0)
 submit = form.form_submit_button('Submit')
 
-completeData = np.array([inputGRE, inputTOEFL, inputUnivRating, 
-                        inputSOP, inputLOR, inputCGPA, inputResearch]).reshape(1, -1)
+completeData = np.array([inputGender, inputAge, inputHyper, 
+                        inputHD, inputGlucose, inputBMI]).reshape(1, -1)
 scaledData = ss_train_test.transform(completeData)
 
 
-st.write('Tekan Submit Untuk Melihat Prediksi Peluang S2 Anda')
+st.write('Tekan Submit Untuk Melihat Hasil Prediksi')
 
 if submit:
-    prediction = l_regressor_ss.predict(scaledData)
+    prediction = m.predict(scaledData)
     if prediction > 1 :
         result = 1
     elif prediction < 0 :
@@ -105,8 +97,6 @@ if submit:
     else :
         result = prediction[0]
     st.write(result*100, "Percent")
-
- 
 
 
 
